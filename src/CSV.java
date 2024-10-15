29abe29c1adfa081d668c0e1d8c238b360ee52f3
feriml <feriml@tsn.at>
2024-10-15 11:12:49 +0200

    CSV Klasse mit export methode erweitert.

--- src/CSV.java
+++ src/CSV.java
@@ -1,4 +1,22 @@
-public class CSV {
+import java.util.ArrayList;
+
+public class CSV extends Export{
 
 
+    public CSV(ArrayList<Artikel> artikelListe) {
+        super(artikelListe);
+    }
+
+    @Override
+    public void export() {
+
+        System.out.println("Nummer;Bezeichnung;Einkaufspreis;Ablaufdatum;Kategorienummer");
+        for (Artikel artikel : getArtikelListe())
+        {
+            System.out.println(artikel.getNummer() +";"+ artikel.getBezeichnung() +";"+
+                    artikel.getEinkaufspreis() +";"+ artikel.getAblaufdatum() +";"+
+                    artikel.getKategorienummer());
+            System.out.println(" ");
+        }
+    }
 }
